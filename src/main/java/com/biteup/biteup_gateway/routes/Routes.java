044e696d8813b9d719c5640be8e4cd3d34bef6a7
AkilaManu <akilamanujith@gmail.com>
2025-04-22 23:01:40 +0530

    create routes

--- src/main/java/com/biteup/biteup_gateway/routes/Routes.java
+++ src/main/java/com/biteup/biteup_gateway/routes/Routes.java
@@ -11,38 +11,59 @@ import org.springframework.web.servlet.function.ServerResponse;
 @Configuration
 public class Routes {
 
-    @Bean
-    public RouterFunction<ServerResponse> productServiceRoute(){
-        return GatewayRouterFunctions.route("product_service")
-        .route(RequestPredicates.path("/api/product"),HandlerFunctions.http("http://localhost:8080"))
-        .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> productServiceRoute() {
+    return GatewayRouterFunctions.route("product_service")
+      .route(
+        RequestPredicates.path("/api/product").or(
+          RequestPredicates.path("/api/product/**")
+        ),
+        HandlerFunctions.http("http://localhost:8080")
+      )
+      .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> OrderServiceRoute(){
-        return GatewayRouterFunctions.route("order_service")
-        .route(RequestPredicates.path("/api/order"),HandlerFunctions.http("http://localhost:8081"))
-        .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> OrderServiceRoute() {
+    return GatewayRouterFunctions.route("order_service")
+      .route(
+        RequestPredicates.path("/api/cart").or(
+          RequestPredicates.path("/api/cart/**")
+        ),
+        HandlerFunctions.http("http://localhost:8081")
+      )
+      .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> RestaurantServiceRoute(){
-        return GatewayRouterFunctions.route("restaurant_service")
-        .route(RequestPredicates.path("/api/restaurant"),HandlerFunctions.http("http://localhost:8082"))
-        .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> RestaurantServiceRoute() {
+    return GatewayRouterFunctions.route("restaurant_service")
+      .route(
+        RequestPredicates.path("/api/restaurant").or(
+          RequestPredicates.path("/api/restaurant/**")
+        ),
+        HandlerFunctions.http("http://localhost:8082")
+      )
+      .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> UserServiceRoute(){
-        return GatewayRouterFunctions.route("user_service")
-        .route(RequestPredicates.path("/api/user/*"),HandlerFunctions.http("http://localhost:8083"))
-        .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> UserServiceRoute() {
+    return GatewayRouterFunctions.route("user_service")
+      .route(
+        RequestPredicates.path("/api/user/*"),
+        HandlerFunctions.http("http://localhost:8083")
+      )
+      .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> paymentServiceRoute(){
-        return GatewayRouterFunctions.route("biteup_payment")
-        .route(RequestPredicates.path("/api/payment/*"),HandlerFunctions.http("http://localhost:8084"))
-        .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> paymentServiceRoute() {
+    return GatewayRouterFunctions.route("biteup_payment")
+      .route(
+        RequestPredicates.path("/api/payment/*"),
+        HandlerFunctions.http("http://localhost:8084")
+      )
+      .build();
+  }
 }
